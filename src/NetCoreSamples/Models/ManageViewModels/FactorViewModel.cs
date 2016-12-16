08398894cdc7c032c445bb81f58d27d6cf504302
Marcelo Hexsel <marcelo.hexsel@hotmail.com>
2016-12-15 22:46:43 -0200

    Add project files.

---
+++ src/NetCoreSamples/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreSamples.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
